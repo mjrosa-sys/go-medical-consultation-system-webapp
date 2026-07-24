3f669ab760f5cc7e3bbc7460a083553216bf7933
mjrosa-sys <maikonrosa2010@hotmail.com>
2026-07-24 18:26:38 -0300

    refactor main package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,11 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/mjrosa-sys/go-medical-consultation-system-webapp/internal/user"
 )
-
-func home(w http.ResponseWriter, r *http.Request) {
-	templates := []string{
-		"./ui/html/base.tmpl",
-		"./ui/html/pages/home.tmpl",
-	}
-
-	ts, err := template.ParseFiles(templates...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-	}
-
-	ts.ExecuteTemplate(w, "base", nil)
-}
 
 func main() {
 	mux := http.NewServeMux()
@@ -27,7 +13,8 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /", home)
+	mux.HandleFunc("GET /{$}", home)
+	mux.HandleFunc("POST /register", user.Register)
 
 	log.Println("Server running localhost:3001")
 
